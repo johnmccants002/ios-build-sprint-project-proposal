759d59641c4eeffa65f42840a7cd347153c27646
Brys1Saclaus2 <brys1saclaus2@gmail.com>
2020-06-24 19:11:37 -0700

    locations feature

---
+++ Group Lunch Coordinator/Group Lunch Coordinator/Controllers/LocationController.swift
@@ -0,0 +1,9 @@
+//
+//  LocationController.swift
+//  Group Lunch Coordinator
+//
+//  Created by B$hady on 6/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
